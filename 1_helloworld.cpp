a9f6317ecdaaf4d56a217b1995fedf714d758e45
kanishkadeo27 <kanishkadeo92022@gmail.com>
2024-12-09 15:28:00 +0530

    basics , variables and datatypes files added

---
+++ 1_helloworld.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout <<"hello world"<<std::endl;
+    std::cout <<"hello kanishka"<<'\n';
+    std::cout <<"hello nityam"<<'\n';
+
+    //this is a single line comment
+    /*
+    this
+    is 
+    a multiline
+    comment
+    */
+    return 0;
+}
---
+++ 2_variables_datatypes.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    x=5;
+
+    int y=6;
+    std::cout<<x<<'\n'<<y<<'\n';
+
+    double a = 10.99;
+    cout<<a<<endl;
+
+    char b = 'B';
+    cout<<b<<endl;
+
+    bool c = true;
+    cout<<c<<endl;
+
+    string name = "kani";
+    cout<<name;
+
+    return 0;
+}
