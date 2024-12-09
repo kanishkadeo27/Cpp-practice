3b04b231fcc401b08b96d0ff7ae10947048f41b4
kanishkadeo27 <kanishkadeo92022@gmail.com>
2024-12-09 19:00:24 +0530

    math functions files added

---
+++ 9_mathFunc.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+#include<cmath>
+
+int main(){
+    double x=3.19;
+    double y=4;
+    double z;
+
+    // z= max(x,y);
+    // z= min(x,y);
+    // z=pow(2,3);
+    // z=sqrt(9);
+    // z=abs(-10);
+    // z=round(x);
+    z=ceil(x);
+    // z=floor(x);
+
+    cout<<z;
+    return 0;
+}
