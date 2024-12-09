9da917e73231af3ac4743b7a256e0284b624654d
kanishkadeo27 <kanishkadeo92022@gmail.com>
2024-12-09 15:46:10 +0530

    added const.cpp

---
+++ 3_const.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    const double PI = 3.14159;
+
+    // PI = 420.69;
+    double radius = 10;
+    double circumference = 2*pi*radius;
+    cout<<circumference<<endl;
+    return 0;
+}
