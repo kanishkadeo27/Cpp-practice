d157ba08516ed59f1141b3e840e35854cecb90ea
kanishkadeo27 <kanishkadeo92022@gmail.com>
2024-12-09 16:41:12 +0530

    arithemetic operators added

---
+++ 6_arithmeticop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int students = 20;
+
+    // students = students+1;
+    // students+=1;
+    students++; 
+    std::cout<<students;
+    return 0;
+    //sames goes with - , * , / , %
+}
