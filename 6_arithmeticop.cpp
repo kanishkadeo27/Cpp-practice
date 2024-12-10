cb50f6d8b03d66eefbc12f80f1091a20883c9acd
kanishkadeo27 <kanishkadeo92022@gmail.com>
2024-12-10 12:23:42 +0530

    operator file updated

--- 6_arithmeticop.cpp
+++ 6_arithmeticop.cpp
@@ -2,12 +2,28 @@
 using namespace std;
 
 int main(){
-    int students = 20;
+    // int students = 20;
 
     // students = students+1;
     // students+=1;
-    students++; 
-    std::cout<<students;
-    return 0;
+    // students++; 
+    // std::cout<<students;
+    // return 0;
     //sames goes with - , * , / , %
+
+    int a , b,res;
+    cout<<"enter the value of a:";
+    cin>>a;
+    cout<<"enter the value of b:";
+    cin>>b;
+    res=a+b;
+    cout<<res<<endl;
+    res=a-b;
+    cout<<res<<endl;
+    res=a*b;
+    cout<<res<<endl;
+    res=a/b;
+    cout<<res<<endl;
+    res=a%b;
+    cout<<res;
 }
