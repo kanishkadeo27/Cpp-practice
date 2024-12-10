1029b1eb45ffa4014055a8efcb011a66a1dbba34
kanishkadeo27 <kanishkadeo92022@gmail.com>
2024-12-10 10:57:57 +0530

    updated variable file

--- 2_variables_datatypes.cpp
+++ 2_variables_datatypes.cpp
@@ -2,23 +2,39 @@
 using namespace std;
 
 int main(){
-    int x;
-    x=5;
+    // int x;
+    // x=5;
 
-    int y=6;
-    std::cout<<x<<'\n'<<y<<'\n';
+    // int y=6;
+    // std::cout<<x<<'\n'<<y<<'\n';
 
-    double a = 10.99;
-    cout<<a<<endl;
+    // double a = 10.99;
+    // cout<<a<<endl;
 
-    char b = 'B';
-    cout<<b<<endl;
+    // char b = 'B';
+    // cout<<b<<endl;
 
-    bool c = true;
-    cout<<c<<endl;
+    // bool c = true;
+    // cout<<c<<endl;
 
-    string name = "kani";
-    cout<<name;
+    // string name = "kani";
+    // cout<<name;
+
+
+    //area of rectangle
+    int l , b , area;
+    l=10;
+    b=20;
+    area = l*b;
+    cout<<"area is : "<<area<<endl;
+    
+    //area of circle
+    const float PI=3.14;
+    // pi = 222/7;
+    int r , area1;
+    r=10;
+    area1 = PI*r*r;
+    cout<<"area of circle is:"<<area1;
 
     return 0;
 }
